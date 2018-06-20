d1a469d42fff39309a4d679078c9f73bc4f4fbff
Daniel Baek <shiningaura@gmail.com>
2018-06-20 16:46:12 -0400

    Added Header File
    Created a header file for preprocessor includes.

---
+++ NumberEntry.h
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * C++ Number Entry                                                            *
+ * =========================================================================== *
+ * Allows for a framework where arrays of numbers can be manipulated by the    *
+ * user.                                                                       *
+ * First, the function will display the current numbers in the array.          *
+ * Second, the function will prompt the user to add or remove or edit numbers  *
+ * or if the user will confirm that the numbers are correct.                   *
+ * For adding, the user will be prompted to enter a number.                    *
+ * For removing, the numbers will be displayed with an index. The user will be *
+ * prompted to select which index to delete.                                   *
+ * For editing, the numbers will be displayed with an index. The user will be  *
+ * prompted to select witch index to edit. Then, the user will be prompted to  *
+ * enter a new number.                                                         *
+ ******************************************************************************/
+// Include guard.
+#ifndef CPPNUMBERENTRY_NUMBERENTRY_H
+#define CPPNUMBERENTRY_NUMBERENTRY_H
+
+#include <vector>
+
+void populate_int_vector(std::vector<int>& intVector);
+
+#endif
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# C-NumberEntry
-A framework for user input of multiple numbers.
+# C++NumberEntry
+A framework for user input of multiple numbers for C++.
+
+## Instructions
+Import header file.
+
+## Contributing
+Please add your name to the CONTRIBUTORS page when contributing.
